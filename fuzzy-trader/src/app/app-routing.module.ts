d733d41f05d759397a6913797add88f2c1702486
M. Joranhezon <mjoranhezon@gmail.com>
2020-07-18 21:51:03 -0300

    Set up home page

--- fuzzy-trader/src/app/app-routing.module.ts
+++ fuzzy-trader/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home-component', component: HomeComponent },
+  { path: '', redirectTo: '/home-component', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home-component'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
