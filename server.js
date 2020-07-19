e2211aef9fcfff626affe60cfc450ad62cf5af75
M. Joranhezon <mjoranhezon@gmail.com>
2020-07-19 19:32:08 -0300

    Move project out of fuzzy-trader folder

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/NOME_DO_SEU_PROJETO'));
+app.use(express.static(__dirname + '/dist/fuzzy-trader'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/NOME_DO_SEU_PROJETO/index.html'));
+res.sendFile(path.join(__dirname+'/dist/fuzzy-trader/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
